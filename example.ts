841c1c5ef6d47c77b3097e04ebdeef434403a04c
lindi0v <lindi0v@lin.di0v>
2026-02-09 14:12:35 +0100

    adjust example

--- example.ts
+++ example.ts
@@ -1,52 +1,54 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 const size = '14px';
+const red = '#ff0000';
 
+// TS syntax highlight still works
 class A {
   private prop: number = 1;
 
   constructor() {}
+
+  someMethod(): void {}
 }
 
 const yourFancyStyle = /* css */ `
 .some {
-  .nested {
-    background: inherit;
-    
+  #id {
+    background: transparent;
+    color: ${red};
   }
-}
-
-#unique {
-  color: #ff0000;
 }
 
 body {
   background: #fff;
-  font-size: ${size ? true : false};
+  font-size: ${size ?? '16px'};
   box-shadow: 0 0 0 3px;
   border-radius: 20px;
   display: flex;
   box-sizing: border-box;
 
+  // just a random complex selector
   [data-some="123"] + abbr ~ div::first-child {
-    width: 100px;
     width: 100px;
   }
 
-  border-radius: 100px;
+  border-radius: 100rem;
 }
 
 html,
 body {
-  box-sizing: border-box;
+  line-height: inherit;
 }
 `;
 
-const otherStyle = /* css */ `
-.another {
-  color: red;
+// other styles works too
+/* css */ `
+.some {
+  backdrop-filter: blur(10px);
   margin: 10px;
 
-  .another {
+  .inner {
     width: 100px;
   }
 }
